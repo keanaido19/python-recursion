f8dfde5302a077c0fa6fd3e636afa5664ade060a
LMS Admin User <lms_admin@wethinkcode.co.za>
2021-11-02 05:01:00 +0000

    Submission Repo Created

---
+++ super_algos.py
@@ -0,0 +1,16 @@
+
+def find_min(element):
+    """TODO: complete for Step 1"""
+    pass
+
+
+def sum_all(element):
+    """TODO: complete for Step 2"""
+    pass
+
+
+def find_possible_strings(character_set, n):
+    """TODO: complete for Step 3"""
+    pass
+
+
